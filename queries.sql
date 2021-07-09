742f91025c7d9ed3c6d302cd3b762cdce43372fc
Matt <matthewearlywine@gmail.com>
2021-07-09 00:00:49 -0500

    Assignment #4 - java-web-dev-techjobs-persistent - Finished first draft

--- queries.sql
+++ queries.sql
@@ -1,10 +1,19 @@
 ## Part 1: Test it with SQL
 
-## Columns and types: int id; employer varchar(255); name varchar(255); skills varchar(255);
-
+Columns and types: int id; employer varchar(255); name varchar(255); skills varchar(255);
 
 ## Part 2: Test it with SQL
 
+Select name
+From employer
+where location = "Saint Louis"
+
 ## Part 3: Test it with SQL
 
+drop table job;
+
 ## Part 4: Test it with SQL
+
+select name, description
+from skill
+order by name asc
--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
@@ -1,13 +1,18 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
 import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
 import javax.validation.constraints.Size;
+import java.util.List;
 
 @Entity
 public class Skill extends AbstractEntity {
 
     @Size(max = 255)
     private String description;
+
+    @ManyToMany(mappedBy="skills")
+    private List<Job> jobs;
 
     public Skill(String description){
         this.description = description;
